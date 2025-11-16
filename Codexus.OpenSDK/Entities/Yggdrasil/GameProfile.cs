259b66128bf179e30c8f426bfc7c81f926687d06
Le4veBadges <2037007221@qq.com>
2025-11-16 08:05:50 +0800

    OpenSource

---
+++ Codexus.OpenSDK/Entities/Yggdrasil/GameProfile.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Codexus.OpenSDK.Entities.Yggdrasil;
+
+public class GameProfile
+{
+    public required string GameId { get; set; }
+    public required string GameVersion { get; set; }
+    public required string BootstrapMd5 { get; set; }
+    public required string DatFileMd5 { get; set; }
+    public required ModList Mods { get; set; }
+    public required UserProfile User { get; set; }
+
+    public string GetModInfo()
+    {
+        return JsonSerializer.Serialize(Mods);
+    }
+}
---
+++ Codexus.OpenSDK/Entities/Yggdrasil/ModList.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Codexus.OpenSDK.Entities.Yggdrasil;
+
+public class ModList
+{
+    [JsonPropertyName("mods")] public List<Mod> Mods { get; set; } = [];
+}
